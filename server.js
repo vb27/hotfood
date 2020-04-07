0929bc930d105b29081446031018374d7e1ba007
Zac Stowell <zgstowell@gmail.com>
2020-04-07 11:05:33 -0700

    server setup

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 // Sets up the Express App
 // =============================================================
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -18,7 +18,24 @@ app.use(express.json());
 
 
 
-
 // Routes
 // ================================================================
 app.get("/")
+
+
+
+
+
+
+
+
+
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
