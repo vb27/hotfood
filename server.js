89159849b9e9906747c5a35025b273c05e579204
vb27 <vampblood87@gmail.com>
2020-04-07 11:53:49 -0700

    if statement when pushing to either the main array or the waitlist

--- server.js
+++ server.js
@@ -13,20 +13,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const tables = [];
+const waitlist = [];
 
 app.post("/api/tables", function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
     var newTable = req.body;
   
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newTable.routeName = newTable.name.replace(/\s+/g, "").toLowerCase();
+    newTable.routeName = newTable.name
   
     console.log(newTable);
   
-    tables.push(newTable);
-  
+    if(tables.length === 5){
+        tables.push(newTable)
+    } else{
+        waitlist.push(newTable)
+    }
     res.json(newTable);
   });
 
